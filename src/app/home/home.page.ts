23c8dac2f1d118cfc708d562de54885ff1fd5aa8
adrien <a.d44@tuta.io>
2023-08-09 14:52:33 +0200

    Bug reproduction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {SwiperOptions} from 'swiper/types';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,89 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('swiperEl', { static: true }) swiperEl: ElementRef | undefined;
+  items = [
+    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },    {
+      label: 'test1',
+    },    {
+      label: 'test2',
+    },    {
+      label: 'test3',
+    },    {
+      label: 'test4',
+    },    {
+      label: 'test5',
+    },    {
+      label: 'test6',
+    },
 
-  constructor() {}
+  ]
 
+  // ngAfterViewInit() {
+  //   const swiperParams: SwiperOptions = {
+  //     initialSlide: 1,
+  //   };
+  //   Object.assign(this.swiperEl?.nativeElement, swiperParams);
+  //   this.swiperEl?.nativeElement.initialize();
+  // }
 }
